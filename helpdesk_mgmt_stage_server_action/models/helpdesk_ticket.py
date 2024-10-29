90c2190616d44b95726b17095056e43e9742f368
Maksym Yankin <yankinmk@gmail.com>
2024-10-29 16:36:46 +0200

    [MIG] helpdesk_mgmt_stage_server_action: Migration to 17.0

--- helpdesk_mgmt_stage_server_action/models/helpdesk_ticket.py
+++ helpdesk_mgmt_stage_server_action/models/helpdesk_ticket.py
@@ -10,31 +10,22 @@ class HelpdeskTicket(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         records = super().create(vals_list)
-        for record in records:
-            action = record.stage_id.action_id
-            if action:
-                context = {
-                    "active_model": self._name,
-                    "active_ids": [record.id],
-                }
-                action.with_context(**context).run()
+        records._run_stage_server_action()
         return records
 
     def write(self, vals):
-        records = "stage_id" in vals and self.filtered(
-            lambda l: l.stage_id.id != vals.get("stage_id")
-        )
-        if records:
-            res = super().write(vals)
-            action = (
-                self.env["helpdesk.ticket.stage"].browse(vals["stage_id"]).action_id
-            )
-            if action:
-                context = {
-                    "active_model": self._name,
-                    "active_ids": records.ids,
-                }
-                action.with_context(**context).run()
-        else:
-            res = super().write(vals)
+        res = super().write(vals)
+        if "stage_id" in vals:
+            self._run_stage_server_action()
         return res
+
+    def _run_stage_server_action(self):
+        for record in self:
+            action_id = record.stage_id.action_id
+            if not action_id:
+                continue
+            ctx = {
+                "active_model": self._name,
+                "active_id": record.id,
+            }
+            action_id.with_context(**ctx).run()
--- helpdesk_mgmt_stage_server_action/tests/test_helpdesk_ticket_stage.py
+++ helpdesk_mgmt_stage_server_action/tests/test_helpdesk_ticket_stage.py
@@ -1,96 +1,45 @@
 # Copyright 2023 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import RecordCapturer, TransactionCase
+
+from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
 
 
-class HelpdeskTicketStageServerAction(SavepointCase):
+class HelpdeskTicketStageServerAction(TransactionCase):
     @classmethod
     def setUpClass(cls):
-        super(HelpdeskTicketStageServerAction, cls).setUpClass()
-        cls.ServerAction = cls.env["ir.actions.server"]
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.HelpdeskTicket = cls.env["helpdesk.ticket"]
         cls.HelpdeskTicketStage = cls.env["helpdesk.ticket.stage"]
-        cls.field = cls.env["ir.model.fields"]._get(cls.HelpdeskTicket._name, "user_id")
-        cls.server_action_helpdesk_ticket = cls.ServerAction.create(
+        cls.HelpdeskTicketTag = cls.env["helpdesk.ticket.tag"]
+        cls.ServerAction = cls.env["ir.actions.server"]
+        cls.create_tag_action = cls.env["ir.actions.server"].create(
             {
-                "name": "Helpdesk Ticket Server Action",
-                "model_id": cls.env.ref("helpdesk_mgmt.model_helpdesk_ticket").id,
-                "state": "object_write",
-                "fields_lines": [
-                    (
-                        0,
-                        0,
-                        {
-                            "col1": cls.field.id,
-                            "evaluation_type": "value",
-                            "value": cls.env.user.id,
-                        },
-                    )
-                ],
+                "model_id": cls.env["ir.model"]._get_id("helpdesk.ticket.tag"),
+                "crud_model_id": cls.env["ir.model"]._get_id("helpdesk.ticket.tag"),
+                "name": "Create new helpdesk tag",
+                "value": "New helpdesk tag",
+                "state": "object_create",
             }
         )
-        cls.helpdesk_ticket_stage_1 = cls.HelpdeskTicketStage.create(
-            {"name": "Stage 1", "sequence": 1}
+        cls.stage_1 = cls.HelpdeskTicketStage.create(
+            {"name": "Stage 1", "sequence": 1, "action_id": cls.create_tag_action.id}
         )
-        cls.helpdesk_ticket_stage_2 = cls.HelpdeskTicketStage.create(
+        cls.ticket = cls.HelpdeskTicket.create(
             {
-                "name": "Stage 2",
-                "action_id": cls.server_action_helpdesk_ticket.id,
-                "sequence": 2,
+                "name": "Create a tag ticket",
+                "description": "Ticket Description",
             }
         )
 
-    def test_helpdesk_ticket_create(self):
-        self.helpdesk_ticket_1 = self.HelpdeskTicket.create(
-            {
-                "name": "Helpdesk Ticket 1",
-                "stage_id": self.helpdesk_ticket_stage_2.id,
-                "description": "Helpdesk Ticket Description",
-            }
+    def test_helpdesk_ticket_run_action(self):
+        self.assertFalse(
+            self.HelpdeskTicketTag.search([("name", "=", "New helpdesk tag")]).exists()
         )
-        self.assertEqual(self.helpdesk_ticket_1.user_id, self.env.user)
-
-    def test_helpdesk_ticket_write(self):
-        self.helpdesk_ticket_2 = self.HelpdeskTicket.create(
-            {
-                "name": "Helpdesk Ticket 2",
-                "stage_id": self.helpdesk_ticket_stage_1.id,
-                "description": "Helpdesk Ticket Description",
-            }
-        )
-        self.helpdesk_ticket_3 = self.HelpdeskTicket.create(
-            {
-                "name": "Helpdesk Ticket 3",
-                "stage_id": self.helpdesk_ticket_stage_1.id,
-                "description": "Helpdesk Ticket Description",
-            }
-        )
-        self.assertNotEqual(self.helpdesk_ticket_2.user_id, self.env.user)
-        self.assertNotEqual(self.helpdesk_ticket_3.user_id, self.env.user)
-        self.helpdesk_ticket_2.write({"stage_id": self.helpdesk_ticket_stage_2.id})
-        self.helpdesk_ticket_3.write({"stage_id": self.helpdesk_ticket_stage_2.id})
-        self.assertEqual(self.helpdesk_ticket_2.user_id, self.env.user)
-        self.assertEqual(self.helpdesk_ticket_3.user_id, self.env.user)
-        self.helpdesk_ticket_3.write({"user_id": False})
-        self.helpdesk_ticket_3.write({"stage_id": self.helpdesk_ticket_stage_2.id})
-        self.assertFalse(self.helpdesk_ticket_3.user_id)
-
-    def test_helpdesk_ticket_without_stage(self):
-        self.helpdesk_ticket_4 = self.HelpdeskTicket.create(
-            {
-                "name": "Helpdesk Ticket 4",
-                "description": "Helpdesk Ticket Description",
-            }
-        )
-        self.assertFalse(self.helpdesk_ticket_4.user_id)
-        self.helpdesk_ticket_stage_3 = self.HelpdeskTicketStage.create(
-            {
-                "name": "Stage 3",
-                "sequence": 3,
-            }
-        )
-        self.helpdesk_ticket_4.write({"stage_id": self.helpdesk_ticket_stage_3.id})
-        self.assertFalse(self.helpdesk_ticket_4.user_id)
-        self.helpdesk_ticket_4.write({"stage_id": self.helpdesk_ticket_stage_2.id})
-        self.assertEqual(self.helpdesk_ticket_4.user_id, self.env.user)
+        with RecordCapturer(self.HelpdeskTicketTag, []) as capture:
+            self.ticket.write({"stage_id": self.stage_1.id})
+        tag = capture.records
+        self.assertEqual(1, len(tag))
+        self.assertEqual("New helpdesk tag", tag.name)
